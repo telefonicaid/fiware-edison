660cc53c0c2ddf2290fe5f15f8d473468e3246d9
Luis <luis.penavillena@telefonica.com>
2015-09-16 15:01:56 +0200

    Refactor

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -10,13 +10,7 @@
 using namespace std;
 
 
-int read() 
-{
-    static int i = 0;
-    return ++i;
-}
-
-string int_to_string(int i) 
+string int_to_string(int i)
 {
     stringstream ss;
     ss << i;
@@ -25,22 +19,17 @@ string int_to_string(int i)
 
 //################ FIWARE VARIABLES ################
 
-string FIWARE_APIKEY = "lzSnQpEsC0lZVOVXaqZK";
+string FIWARE_APIKEY = "xxxxxxx";
 string FIWARE_DEVICE = "myEdison";
-string FIWARE_SERVER = "test.ttcloud.net";
+string FIWARE_SERVER = "hackathon.ttcloud.net";
 string FIWARE_PORT = "8082";
 string url="http://"+FIWARE_SERVER+":"+FIWARE_PORT+"/iot/d?i="+FIWARE_DEVICE+"&k="+FIWARE_APIKEY;
 
 string body="";
 
-void readsensors()
+void readsensors(mraa::Aio* a0_pin, mraa::Aio* a1_pin)
 {
-	// check that we are running on Galileo or Edison
-	mraa_platform_t platform = mraa_get_platform_type();
 
-	// create an analog input object for each sensor
-	mraa::Aio* a0_pin = new mraa::Aio(0);
-	mraa::Aio* a1_pin = new mraa::Aio(1);
 	// Dictionary to persist Sensor values
 	typedef map <string, string> Table;
 	Table measures;
@@ -69,44 +58,51 @@ void readsensors()
 }
 
 void postmeasures(){
+		CURL *curl;
+		CURLcode res;
 
-	CURL *curl;
+		char bodyChar[body.length()];
+		strcpy(bodyChar, body.c_str());
+		//probar a meter directamente body.c_str() y url.c_str()
 
-	string url="http://"+FIWARE_SERVER+":"+FIWARE_PORT+"/iot/d?i="+FIWARE_DEVICE+"&k="+FIWARE_APIKEY;
-
-	char bodyChar[body.length()];
-	strcpy(bodyChar, body.c_str());
-
-	char urlChar[url.length()];
-	strcpy(urlChar, url.c_str());
+		char urlChar[url.length()];
+		strcpy(urlChar, url.c_str());
 
 
-	curl = curl_easy_init();
-	
-	//Send body to IoT Stack platform
-	if(curl) {
-		cout << "Sending -> " << body << endl;
-	
-		curl_easy_setopt(curl, CURLOPT_URL, urlChar);
-		struct curl_slist *headers=NULL;
-		headers = curl_slist_append(headers, "Content-Type:");
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, bodyChar);
-	
-		curl_easy_perform(curl);
-	}
-	curl_global_cleanup();
-	// Clean body before the following iteration
-	body="";
+		curl = curl_easy_init();
+
+		//Send body to IoT Stack platform
+		if(curl) {
+			cout << "Sending: " << body << endl;
+
+			curl_easy_setopt(curl, CURLOPT_URL, urlChar);
+			struct curl_slist *headers=NULL;
+			headers = curl_slist_append(headers, "Content-Type:");
+			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, bodyChar);
+
+			res = curl_easy_perform(curl);
+			if(res != CURLE_OK)
+				cout << "Sending measures failed " << endl;
+		}
+		curl_easy_cleanup(curl);
+		// Clean body before the following iteration
+		body="";
 
 }
 
 int main()
 {
+	// check that we are running on Galileo or Edison
+		mraa_platform_t platform = mraa_get_platform_type();
+	// create an analog input object for each sensor
+		mraa::Aio* a0_pin = new mraa::Aio(0);
+		mraa::Aio* a1_pin = new mraa::Aio(1);
+
 	// loop forever printing the input value every second
 	for (;;) {
 
-		readsensors();
+		readsensors(a0_pin, a1_pin);
 		postmeasures();
 		sleep(1);
 	}
