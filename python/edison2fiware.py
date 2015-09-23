d1e64aae024643186e45c8d189d50a351ca346e0
Luis <luis.penavillena@telefonica.com>
2015-09-23 10:48:30 +0200

    commit changes

--- python/edison2fiware.py
+++ python/edison2fiware.py
@@ -5,11 +5,12 @@
 import mraa
 import time
 import requests
+import json
 
 ################ FIWARE VARIABLES ################
-FIWARE_SERVER = "test.ttcloud.net"
+FIWARE_SERVER = "hackathon.ttcloud.net"
 FIWARE_PORT = "8082"
-FIWARE_APIKEY = "xxxxxx"
+FIWARE_APIKEY = "93d5810871197ae37c3d7f1f69e06a92"
 FIWARE_DEVICE = "myEdison"
 
 #Time between measures
@@ -20,6 +21,10 @@ luminosity = mraa.Aio(0)
 
 #Setup access to analog input #1 (A1) connected to Button Sensor
 pulse = mraa.Aio(1)
+
+#commands configuration
+led = mraa.Gpio(6)
+led.dir(mraa.DIR_OUT)
 
 #Setup dictionary to include measures
 measures = {}
@@ -36,20 +41,46 @@ def readMeasures():
 
 def postMeasures():
 
+    first = True
     payload = ""
+    received_commands=[]
     for key,value in measures.items():
-        #Measures payload format is alias1|value1#alias2|value2#...#aliasN|valueN
+        #Measures payload format is alias1|value1#alias2|value2#....
         #Add measures separator "#" to the request payload only when it is not the first measure
-        if(payload != ""):
+        if(first):
+            first = False
+        else:
             payload = payload + "#"
+
         payload = payload + key + "|" + value
 
     print('Sending measures to FIWARE IoT Stack '+payload)
 
     url = "http://" + FIWARE_SERVER+ ":" +FIWARE_PORT+ "/iot/d"
-    querystring = {"i": FIWARE_DEVICE, "k": FIWARE_APIKEY}
+    querystring = {"i": FIWARE_DEVICE, "k": FIWARE_APIKEY, "getCmd":"1"}
     r = requests.post(url, data=payload, params=querystring)
     print "Response Status Code: "+str(r.status_code)
+    if r.text != "":
+        response = r.text
+        print response
+        received_commands = response.split("#")
+        received_commands.reverse()
+        while (received_commands!=[]):
+            command_name, command_value = received_commands.pop().split("@")[1].split("|")
+            print command_value
+            if command_name == "ledr":
+                led.write(1) if command_value == "on" else led.write(0)
+                sendAck()
+            time.sleep(1)
+            
+    
+def sendAck():
+    
+    url = "http://" + FIWARE_SERVER+ ":" +FIWARE_PORT+ "/iot/d"
+    querystring = {"i":FIWARE_DEVICE,"k":FIWARE_APIKEY}
+
+    payload = FIWARE_DEVICE+"@ledr|OK"
+    response = requests.request("POST", url, data=payload, params=querystring)
 
 ## MAIN ##
 while True:
